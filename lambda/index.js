15e7fdbe821c6e99147efda592c187a442a1a150
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-08-06 07:19:39 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -10,7 +10,23 @@ const LaunchRequestHandler = {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
     },
     handle(handlerInput) {
-        const speakOutput = 'Welcome, you can say Hello or Help. Which would you like to try?';
+        let speakOutput = 'Benvenuti sul PlayStation Store. Vuoi cercare un gioco?';
+        
+        const supportedInterfaces = Alexa.getSupportedInterfaces(handlerInput.requestEnvelope);
+        const htmlInterface = supportedInterfaces['Alexa.Presentation.HTML'];
+        if(htmlInterface !== null && htmlInterface !== undefined) {
+            speakOutput = "Benvenuti sul PlayStation Store. Vuoi cercare un gioco da schermo?";
+            handlerInput.responseBuilder.addDirective({
+              type: "Alexa.Presentation.HTML.Start",
+              request: {
+                uri: "https://94a9-2-43-255-194.eu.ngrok.io/",
+                method: "GET",
+              },
+              configuration: {
+                  timeoutInSeconds: 300
+              }
+            });
+        }
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
