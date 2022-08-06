4ca5f1964290493c4011622cdbb8c18fe915802d
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-08-06 11:51:29 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -1,7 +1,7 @@
 const Alexa = require('ask-sdk-core');
 
 const {
-  LaunchRequestHandler,
+  //LaunchRequestHandler,
   HelloWorldIntentHandler,
   HelpIntentHandler,
   CancelAndStopIntentHandler,
@@ -11,6 +11,40 @@ const {
   ErrorHandler,
 } = require('./handlers');
 
+const LaunchRequestHandler = {
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest'
+    );
+  },
+  handle(handlerInput) {
+    let speakOutput = 'Benvenuti sul PlayStation Store. Vuoi cercare un gioco?';
+
+    const supportedInterfaces = Alexa.getSupportedInterfaces(
+      handlerInput.requestEnvelope
+    );
+    const htmlInterface = supportedInterfaces['Alexa.Presentation.HTML'];
+    if (htmlInterface !== null && htmlInterface !== undefined) {
+      speakOutput = 'Benvenuti sul PlayStation Store. Vuoi cercare un gioco?';
+      handlerInput.responseBuilder.addDirective({
+        type: 'Alexa.Presentation.HTML.Start',
+        request: {
+          uri: 'https://b32c-2-43-255-194.eu.ngrok.io',
+          method: 'GET',
+        },
+        configuration: {
+          timeoutInSeconds: 300,
+        },
+      });
+    }
+
+    return handlerInput.responseBuilder
+      .speak(speakOutput)
+      .reprompt(speakOutput)
+      .getResponse();
+  },
+};
+
 /**
  * This handler acts as the entry point for your skill, routing all request and response
  * payloads to the handlers above. Make sure any new handlers or interceptors you've
@@ -18,7 +52,7 @@ const {
  * */
 exports.handler = Alexa.SkillBuilders.custom()
   .addRequestHandlers(
-    LaunchRequestHandler(Alexa),
+    LaunchRequestHandler,
     HelloWorldIntentHandler(Alexa),
     HelpIntentHandler(Alexa),
     CancelAndStopIntentHandler(Alexa),
