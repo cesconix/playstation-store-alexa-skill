2dc90948b8b7e1fe738019c2cd937c195435d9e5
Francesco Pasqua <cesconix@me.com>
2022-08-09 12:48:31 +0200

    feat: change message to launch intent

--- lambda/handlers/LaunchRequestHandler.js
+++ lambda/handlers/LaunchRequestHandler.js
@@ -8,7 +8,8 @@ const LaunchRequestHandler = (Alexa) => ({
     );
   },
   handle(handlerInput) {
-    let speakOutput = 'Benvenuti sul PlayStation Store.';
+    let speakOutput =
+      'Benvenuti su PlayStation Store. Se ti serve poter cercare videogiochi, per titolo o categoria, comprarli o sapere loro specifiche, puoi chiedere a me.';
 
     handlerInput.responseBuilder.addDirective({
       type: 'Alexa.Presentation.HTML.Start',
